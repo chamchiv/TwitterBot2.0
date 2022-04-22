2a15a6f528b98252e7147f4a14a1e0f754cdfa98
chamchiv <chamroeunrathchiv@gmail.com>
2022-04-22 10:41:07 -0400

    new one

--- twitterbot.py
+++ twitterbot.py
@@ -1,19 +1,36 @@
 import tweepy
 import time
 import random
+import os
 #info for the keys and secret
 from info import *
-
+from datetime import datetime
 
 #authorisation stuff
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(key,secret)
 api = tweepy.API(auth)
 
+#media folder basically read it and store it into a txt file
+memes = os.listdir("C:/TweepyMemes")
+random.choice(memes)
+
+
+#media randomiser
+
+
+
+#proverb randomiser
+plines = open("proverbs").read().splitlines()
+rand_plines = random.choice(plines)
+
+
 #paths to the image folder and the images
 path= 'C:/TweepyMedia/'
-picture = 'gigachad.jpg'
+picture = 'gigachad'
 filename = path + picture
+
+
 
 
 
@@ -31,23 +48,33 @@ def store_last_seen(name_list,last_seen_id):
     return
 
 
-def lyrics_reply():
+def reply():
     #this is to grab the tweet
     tweet = api.mentions_timeline(since_id=read_last_seen(name_list))
     lines = open("Lyrics").read().splitlines()
-    plines=open("proverbs").read().splitlines()
-    rand_plines = random.choice(plines)
     rand_lines = random.choice(lines)
     for tweets in (tweet):
         if "#lyrics" in tweets.text.lower():
-         print(str(tweets.id) + "" + tweets.text)
+         print(str(tweets.id) + " " + tweets.text) #basically this just shows that its receiving the tweet
          api.update_status_with_media("@" + tweets.user.screen_name + " " + rand_lines,filename, in_reply_to_status_id = tweets.id_str)
-        if "proverbs" in tweets.text.lower():
-         print(str(tweets.id) + "" + tweets.text)
+        if "#proverbs" in tweets.text.lower():
+         print(str(tweets.id) + " " + tweets.text) #basically this just shows that its receiving the tweet
          api.update_status_with_media("@" + tweets.user.screen_name + " " + rand_plines, filename, in_reply_to_status_id=tweets.id_str)
-
+        if "memes" in tweets.text.lower():
+            api.update_status_with_media("@" + tweets.user.screen_name + " " , random.choice(memes),
+                                         in_reply_to_status_id=tweets.id_str)
         store_last_seen(name_list, tweets.id)
 
+
+#setting up the fucntion to check the time
+minutes = datetime.now().minute
+def Hourtweet(minutes):
+     if minutes == 0:
+         api.update_status_with_media(rand_plines,filename)
+
+
 while True:
-    lyrics_reply()
-    time.sleep(15)
+    reply()
+    time.sleep(5)
+    Hourtweet(minutes)
+    time.sleep(5)
